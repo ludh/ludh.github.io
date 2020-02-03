a44b70c4ab74187ddccfba9cd6bfa9173ab7fe46
Gabriel Reis <gabrielcnr@gmail.com>
2020-02-03 00:00:35 +0000

    using Pelican

---
+++ content/pages/services.md
@@ -0,0 +1,11 @@
+Title: Services
+Date: 2020-01-29 10:20
+Category: Services
+
+## Software Development Consultancy
+
+We provide especialised consultancy in software development projects. Our professional developers have been involved in delivering successful software projects, across many different industries, over the past two decades.
+
+## Python Training
+
+We offer a bespoke a-la-carte Software Development with Python to other companies and professionals.
---
+++ pelicanconf.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Gabriel Reis'
+SITENAME = 'LUDH Limited'
+# SITEURL = 'www.ludh.co.uk'
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/London'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ('Pelican', 'http://getpelican.com/'),
+    ('Python.org', 'http://python.org/'),
+)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+
+# THEME = '/Users/gabrielreis/workspace/ludh.github.io/free-agent'
